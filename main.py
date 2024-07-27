dbf0b698bb709a2d9dddea97af24bff7e7afba77
OlgaGrenc <olga.grenc@mail.ru>
2024-07-27 20:26:35 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+print('"1st program"')
+print(9 ** 0.5 * 5)
+
+print('"2nd program"')
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+print('"3rd program"')
+a = 1234
+b = 5678
+c = a % 1000 // 10
+d = b % 1000 // 10
+print(c+d)
+
+print('"4th program"')
+a = 13.42
+a1 = a * 100 // 100
+b = 42.13
+b1 = b * 100 % 100
+print(a1 == b1)

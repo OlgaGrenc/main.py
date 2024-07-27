8876f04d251858d71bd0e7e3897b9ba573cae7f0
OlgaGrenc <olga.grenc@mail.ru>
2024-07-28 00:01:55 +0500

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,5 +1,5 @@
 count_hw = 12
 count_h = 1.5
-time_of_hw = count_hw * count_h
+time_of_hw = count_h/ count_hw
 name_c = 'Python'
 print("Курс:", name_c, ",", "всего задач:", count_hw, ",", "затрачено часов:", count_h, ",", "среднее время выполнения", time_of_hw, "часa.")

507ef676fbe3e02246fc270992b71c4e566d37af
OlgaGrenc <olga.grenc@mail.ru>
2024-08-01 00:07:57 +0500

    Add files via upload

--- module1hard.py
+++ module1hard.py
@@ -3,20 +3,7 @@ students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 # grades by alphabet
 # students are in a random order
 # right decision: {'Aaron': 4.0, 'Bilbo': 2.25, 'Johhny': 4.0, 'Khendrik': 3.6666666666666665, 'Steve': 4.8}
-print("Количество учеников ", len(grades))
-print("Количество оценок каждого ", len(grades[0]), len(grades[1]), len(grades[2]), len(grades[3]), len(grades[4]))
-print("Общий балл каждого ученика ", sum(grades[0]), sum(grades[1]), sum(grades[2]), sum(grades[3]), sum(grades[4]))
-A = sum(grades[0])/len(grades[0])
-B = sum(grades[1])/len(grades[1])
-C = sum(grades[2])/len(grades[2])
-D = sum(grades[3])/len(grades[3])
-F = sum(grades[4])/len(grades[4])
-grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
-students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 s = students
-# grades by alphabet
-# students are in a random order
-# right decision: {'Aaron': 4.0, 'Bilbo': 2.25, 'Johhny': 4.0, 'Khendrik': 3.6666666666666665, 'Steve': 4.8}
 print("Количество учеников(по алфавиту) ", len(grades))
 print("Количество оценок каждого ", len(grades[0]), len(grades[1]), len(grades[2]), len(grades[3]), len(grades[4]))
 print("Общий балл каждого ученика ", sum(grades[0]), sum(grades[1]), sum(grades[2]), sum(grades[3]), sum(grades[4]))

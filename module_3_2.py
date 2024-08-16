eeca6c1fd3c0e621e53687ac20518e5b57bbcb4c
OlgaGrenc <olga.grenc@mail.ru>
2024-08-16 13:41:28 +0500

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -5,15 +5,15 @@
 # Проверка на отправку самому себе.
 # Проверка на отправителя по умолчанию.
 def send_email(message, recipient, *, sender="university.help@gmail.com"):
-    if sender == recipient:
-        print("Нельзя отправить письмо самому себе!")
-    elif sender == 'university.help@gmail.com':
-        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
-    elif '@' or ".com" or '.ru' or '.net' not in recipient and sender:
+    if "@" not in recipient and sender\
+            or not recipient.endswith((".com", '.ru', '.net')) or not sender.endswith((".com", '.ru', '.net')):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+    elif sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
     elif sender != 'university.help@gmail.com':
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
-
+    else:
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
 
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')

c429bb2762469109293ea4d9efb87af16f541e20
OlgaGrenc <olga.grenc@mail.ru>
2024-11-30 17:04:05 +0500

    Add files via upload

---
+++ main_repeat.py
@@ -0,0 +1,12 @@
+# "1st program"
+print(9 ** 0.5 * 5)
+# "2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+# "3rd program"
+print(a := (2+2*2))
+print(b := ((2+2)*2))
+print(a == b)
+# "4th program"
+s = "123.456"
+s = int(float(s) * 10) % 10
+print(s)
